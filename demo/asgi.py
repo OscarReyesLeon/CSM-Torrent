3d4176d9f89cea22b33599cdf74336c90abea640
Oscar Reyes León <oscar@mercax.com>
2022-06-28 23:49:18 -0500

    recycled base

--- demo/asgi.py
+++ demo/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for demo project.
+ASGI config for proyecto project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- demo/wsgi.py
+++ demo/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for demo project.
+WSGI config for proyecto project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
